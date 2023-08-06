3b0d3e757e3ab9b2cbdba87e27446ea7d205e6a6
tatukristiani <tatu.pulkkinen@metropolia.fi>
2023-08-06 23:16:05 +0300

    change api url

--- src/utility/axios.js
+++ src/utility/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:4000/"  //For local testing 
-    //baseURL: "https://shop-scraper-react.herokuapp.com/" // Herokus base URL for the backend calls 
+    //baseURL: "http://localhost:4000/"  //For local testing 
+    baseURL: "https://shop-scraper-backend-9bdcee7fae35.herokuapp.com//" // Herokus base URL for the backend calls 
 });
